0b29306c06e48f941c9cbec02113ea33a2eb0245
Tawan <62010308@kmitl.ac.th>
2022-07-13 15:08:32 +0700

    Add overlay navbar when click burger menu in mobile frame

---
+++ app.js
@@ -0,0 +1,7 @@
+function openNav() {
+    document.getElementById("overlay-nav").style.width = "100%";
+  }
+  
+  function closeNav() {
+    document.getElementById("overlay-nav").style.width = "0%";
+  }
