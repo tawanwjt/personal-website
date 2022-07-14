d5f8cd3efd902c23a78c5f074913086f0bae11ed
Tawan <62010308@kmitl.ac.th>
2022-07-14 16:16:17 +0700

    Add effect to school img and edit imported-font link

--- app.js
+++ app.js
@@ -2,6 +2,34 @@ function openNav() {
     document.getElementById("overlay-nav").style.width = "100%";
   }
   
-  function closeNav() {
-    document.getElementById("overlay-nav").style.width = "0%";
-  }
+function closeNav() {
+  document.getElementById("overlay-nav").style.width = "0%";
+}
+
+function showTimeline_K() {
+  var pic = document.getElementById("pic-K");
+  var timeline = document.getElementById("timeline-K");
+  pic.style.display = "none";
+  timeline.style.display = "block";
+}
+
+function showTimeline_D() {
+  var pic = document.getElementById("pic-D");
+  var timeline = document.getElementById("timeline-D");
+  pic.style.display = "none";
+  timeline.style.display = "block";
+}
+
+function showPic_K() {
+  var pic = document.getElementById("pic-K");
+  var timeline = document.getElementById("timeline-K");
+  pic.style.display = "block";
+  timeline.style.display = "none";
+}
+
+function showPic_D() {
+  var pic = document.getElementById("pic-D");
+  var timeline = document.getElementById("timeline-D");
+  pic.style.display = "block";
+  timeline.style.display = "none";
+}
